862aebdb47852dc285ab60eebc7b9c7790674d5a
“boray” <bsaygilier33@gmail.com>
2021-12-19 17:59:44 +0300

    dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import App from 'next/app'
+import Head from 'next/head'
+import '../styles.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default class Root extends App {
+  render() {
+    const { Component } = this.props
+
+    return (
+      <>
+        <Head>
+          <title>PalPay</title>
+        </Head>
+
+        <Component />
+      </>
+    )
+  }
 }
-
-export default MyApp
